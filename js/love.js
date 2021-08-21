2a9e22505340455682c219f801c81cd1e5fd136a
Oginni Emmanuel Olusegun <79214437+esao019@users.noreply.github.com>
2021-08-21 02:57:54 +0100

    show others love
    tell others u love them

---
+++ js/love.js
@@ -0,0 +1,12 @@
+
+/*
+
+let love = document.querySelector(".small_pics")
+const image = document.querySelector(".screenshot")
+love.addEventListener("click",(e) =>{
+    let tag = e.target;
+
+    if (tag.classList.contains("d-block")){
+ tag.classList.add("expand")
+    }
+})
